a856b539cf66a5b226842bd421e4a15fb10d6131
Kajal Shukla <kajalshukla0123@gmail.com>
2023-01-21 00:17:17 +0530

    first commit

---
+++ scripts/finalDeploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+    const chai = await hre.ethers.getContractFactory("chai");
+    const contract = await chai.deploy(); 
+  
+    await contract.deployed();
+    console.log("Address of contract:", contract.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
